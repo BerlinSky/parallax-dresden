67bf332f7668c359619fef757c56eddc2a14da09
Berlin Sky <pedroyoung8989@gmail.com>
2017-05-28 09:39:54 -0700

    animate photo gallery

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 import 'jquery';
 
 $(function () {
-  $(window).scroll(function(){
+  animateHeaderBox();
+  animatePhotoGallery();
+});
 
+const animateHeaderBox = () => {
+  $(window).scroll(function() {
     const wScroll = $(this).scrollTop();
 
     $('.headerParallaxBox__backBanner').css({
@@ -16,7 +20,20 @@ $(function () {
     $('.headerParallaxBox__frontBanner').css({
       'transform' : 'translate(0px, -'+ wScroll /9 +'%)'
     });
-
   });
-});
+}
 
+const animatePhotoGallery = () => {
+  $(window).scroll(function() {
+    const wScroll = $(this).scrollTop();
+
+    // if(wScroll > $('.photoGallery__photoFrame').offset().top - ($(window).height() / 2)) {
+    if(wScroll > $('.photoGallery__photoFrame').offset().top - 350) {
+      $('.photoGallery__photo').each(function(i) {
+        setTimeout(function(){
+          $('.photoGallery__photo').eq(i).addClass('photoGallery__photo--show');
+        }, 175 * ( i + 1.5 ));
+      });
+    }
+  })
+}
