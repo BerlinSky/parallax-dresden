44d4a4654e2283a9245e0719e398407308d2ed7c
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-04 12:17:15 -0700

    images attached

--- webpack.config.js
+++ webpack.config.js
@@ -132,6 +132,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       favicon: 'app/favicon.png',
       chunnk: ['index'],
+      template: 'app/post.pug',
+      filename: 'post.html'
+    }),
+    new HtmlWebpackPlugin({
+      favicon: 'app/favicon.png',
+      chunnk: ['index'],
       template: 'app/service.pug',
       filename: 'service.html'
     }),
