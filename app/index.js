a92e065b076ecf92145e576dbd2a3e7180aefbf0
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-20 13:52:46 -0700

    set up

--- app/index.js
+++ app/index.js
@@ -2,4 +2,24 @@ import 'jquery';
 
 $(function () {
   console.log("enjoy parallax");
+  animateSectionImage();
 });
+
+const animateSectionImage = () => {
+  // const container = $('.section.sectionImage');
+
+  $('.l-contentContainer').scroll(function() {
+    const wScroll = $(this).scrollTop();
+   
+    const topPos = (wScroll < 100) ? (100 - wScroll) : 0;
+    const size = (wScroll/8) + '%' + ' ' + (wScroll/8) + '%';
+
+    console.log(topPos);
+
+    $('.sectionImage__overlay').css({
+      'top': topPos +'%',
+      'background-size': size
+    });
+
+  });
+}
