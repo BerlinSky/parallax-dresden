2a1b671b119176a8cfbc002aa02ce551310ea63f
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-25 16:40:58 -0700

    update

--- app/index.js
+++ app/index.js
@@ -1,7 +1,29 @@
 import 'jquery';
 
-import { windowScrolling } from './js/animation/scrolling';
+// import { windowScrolling } from './js/animation/scrolling';
 
 $(function () {
-   window.addEventListener("scroll", windowScrolling, false);
+   window.addEventListener("scroll", scrollLoop, false);
+  //  window.addEventListener("scroll", windowScrolling, false);
+
+  // window.addEventListener("DOMContentLoaded", scrollLoop, false);
 });
+
+const scrollLoop = () => {
+  const posX = window.scrollX;
+  const posY = window.scrollY;
+
+  const circle = document.querySelector(".shape.shape--circle");
+  const square = document.querySelector(".shape.shape--square");
+  const pentagon = document.querySelector(".shape.shape--pentagon");
+
+  translateElem(circle, posX, posY * -0.2);
+  translateElem(square, posX, posY * -1.5);
+  translateElem(pentagon, posX, posY * -5);
+
+  // requestAnimationFrame(scrollLoop);
+}
+
+const translateElem = (elem, posX, posY) => {
+  elem.style.transform = ` translate3d(${posX}px, ${posY}px, 0)`;
+}
