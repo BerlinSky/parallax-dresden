2be23e07e5263a816282e3687c3e1b525ecb4527
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-25 23:10:57 -0700

    flying out menu

--- app/index.js
+++ app/index.js
@@ -1,30 +1,8 @@
 import 'jquery';
 
+import { handleMobileMenu } from './js/animation/flyout-menu';
+
 $(function () {
-  // windowParallaxScrolling();
   handleMobileMenu();
 });
 
-const hideMenu = (e) => { 
-  const flyoutMenu = document.querySelector('.flyoutMenu');
-  flyoutMenu.classList.remove('show');
-  e.stopPropagation();
-
-  document.body.style.overflowY = "auto";
-}
-
-const showMenu = (e) => {
-  const flyoutMenu = document.querySelector('.flyoutMenu');
-  flyoutMenu.classList.add('show');
-  e.stopPropagation();
-
-  document.body.style.overflowY = "hidden";
-}
-
-const menuButtonShow = document.querySelector('.js-menuButton-show');
-const menuButtonHide = document.querySelector('.js-menuButton-hide');
-
-export const handleMobileMenu = () => {
-  menuButtonShow.addEventListener('click', showMenu, false);
-  menuButtonHide.addEventListener('click', hideMenu, false);
-}
