6e7c5162d9fd67a5adeec58f65efead674019c6d
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-26 17:18:27 -0700

    update

--- app/js/animation/parallax-scrolling.js
+++ app/js/animation/parallax-scrolling.js
@@ -2,6 +2,8 @@ const parallaxScroll = () => {
   const posX = window.scrollX;
   const posY = window.scrollY;
 
+  const imageBgNew = document.querySelector(".imageContent.imageContent--news");
+
   const circle = document.querySelector(".shape.shape--circle");
   const square = document.querySelector(".shape.shape--square");
   const pentagon = document.querySelector(".shape.shape--pentagon");
@@ -9,6 +11,8 @@ const parallaxScroll = () => {
   translateElem(circle, posX, posY * -0.1);
   translateElem(square, posX, posY * -2.0);
   translateElem(pentagon, posX, posY * .5);
+
+  translateElem(imageBgNew, posX, posY * -0.5);
 
   requestAnimationFrame(parallaxScroll);
 }
