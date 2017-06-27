9b1e5f03307ca141221995185a297b78c32c0bc8
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-27 15:42:58 -0700

    update

--- app/js/animation/parallax-scrolling.js
+++ app/js/animation/parallax-scrolling.js
@@ -5,7 +5,6 @@ const parallaxScroll = () => {
   const posX = window.scrollX;
   const posY = window.scrollY;
 
-
   const circle = document.querySelector(".shape.shape--circle");
   const square = document.querySelector(".shape.shape--square");
   const pentagon = document.querySelector(".shape.shape--pentagon");
@@ -31,31 +30,16 @@ export const windowParallaxScrolling = () => {
 export const setTopPositions = () => {
   steckImagesVertically(imageSlider, imageBgNews);
 
-  // var img = new Image();
-  // img.src = window.getComputedStyle(imageSlider).getPropertyValue("background-image");
-
-  // img.src = window.getComputedStyle(imageSlider).getPropertyValue("background-image").replace(/url\(|\)$/ig, "");
-
   function resize() {
-
-    console.log("resized");
-
-
     const img = new Image;
     img.src = $(imageSlider).css('background-image').replace("url(", "").replace(")", "").replace("\"", "").replace("\"", "");
-    // var bgHeight = img.height;
-    // var bgHeight  = img.height + 'px';
 
-// const size = $(imageSlider).css('background-size');
-
- var bgHeight = document.body.offsetWidth * img.height / img.width;
-    console.log(bgHeight);
-
+    const bgHeight = document.body.offsetWidth * img.height / img.width;
     $(imageBgNews).css({ 'top': bgHeight} );
-    
   }
-  window.onresize = resize; resize();
 
+  window.onresize = resize; 
+  resize();
 }
 
 const steckImagesVertically = (elem1, elem2) => {
@@ -63,20 +47,7 @@ const steckImagesVertically = (elem1, elem2) => {
   img.src = $(elem1).css('background-image').replace("url(", "").replace(")", "").replace("\"", "").replace("\"", "");
 
   $(img).on('load', function() {
-      // var bgImgWidth = img.width;
-      // var bgImgHeight = img.height + "px";
-
- var bgHeight = document.body.offsetWidth * img.height / img.width;
-      
-      // console.log("w::"+bgImgWidth+",h::"+bgImgHeight) ;
-
-      $(elem2).css({ 'top': bgHeight} );
+    const bgHeight = document.body.offsetWidth * img.height / img.width;
+    $(elem2).css({ 'top': bgHeight} );
   });
-
-  // $(window).on("resize", function () {
-  //     // $('body').height($('body').width() * img.height / img.width);
-  //     var bgImgHeight = img.height;
-  //     $(elem2).css({ 'top': bgImgHeight} );
-  // }).resize();
-  
 }
