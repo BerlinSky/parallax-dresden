716b3b24abea914aa19a7363dcad3415c0c13d68
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-27 23:07:13 -0700

    update

--- app/js/animation/parallax-scrolling.js
+++ app/js/animation/parallax-scrolling.js
@@ -1,7 +1,8 @@
 const imageSlider = document.querySelector(".imageContent.imageContent--slider");
-const imageBgNews = document.querySelector(".imageContent.imageContent--news");
-const imageBgTour = document.querySelector(".imageContent.imageContent--tour");
-const imageBgVideo = document.querySelector(".imageContent.imageContent--video");
+const parallaxWrapper = document.querySelector(".parallaxWrapper");
+// const imageBgNews = document.querySelector(".imageContent.imageContent--news");
+// const imageBgTour = document.querySelector(".imageContent.imageContent--tour");
+// const imageBgVideo = document.querySelector(".imageContent.imageContent--video");
 
 const parallaxScroll = () => {
   const posX = window.scrollX;
@@ -15,9 +16,12 @@ const parallaxScroll = () => {
   translateElem(square, posX, posY * -2.0);
   translateElem(pentagon, posX, posY * .5);
 
-  translateElem(imageBgTour, posX, posY * -0.5);
-  translateElem(imageBgNews, posX, posY * -0.5);
-  translateElem(imageBgVideo, posX, posY * -0.5);
+  translateElem(parallaxWrapper, posX, posY * -0.5);
+
+  // translateElem(imageBgTour, posX, posY * -0.5);
+
+  // translateElem(imageBgNews, posX, posY * -0.5);
+  // translateElem(imageBgVideo, posX, posY * -0.5);
 
   requestAnimationFrame(parallaxScroll);
 }
@@ -32,9 +36,10 @@ export const windowParallaxScrolling = () => {
 }
 
 export const setTopPositions = () => {
-  steckImagesVertically(imageSlider, imageBgTour);
-  steckImagesVertically(imageBgTour, imageBgNews);
-  steckImagesVertically(imageBgNews, imageBgVideo);
+  steckImagesVertically(imageSlider, parallaxWrapper);
+
+  // steckImagesVertically(imageBgTour, imageBgNews);
+  // steckImagesVertically(imageBgNews, imageBgVideo);
 
   // function resize() {
   //   const img = new Image;
@@ -52,12 +57,13 @@ export const setTopPositions = () => {
     $(elem2).css({ 'top': bgHeight} );
   }
 
-  window.onresize = resize(imageSlider, imageBgTour); 
-  window.onresize = resize(imageBgTour, imageBgNews); 
-  window.onresize = resize(imageBgNews, imageBgVideo); 
-  resize(imageSlider, imageBgTour);
-  resize(imageBgTour, imageBgNews);
-  resize(imageBgNews, imageBgVideo);
+  // window.onresize = resize(imageSlider, imageBgTour); 
+  // window.onresize = resize(imageBgTour, imageBgNews); 
+  // window.onresize = resize(imageBgNews, imageBgVideo); 
+  window.onresize = resize(imageSlider, parallaxWrapper); 
+  resize(imageSlider, parallaxWrapper);
+  // resize(imageBgTour, imageBgNews);
+  // resize(imageBgNews, imageBgVideo);
 }
 
 
